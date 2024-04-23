09746f989db82fcd3817f4be91ec2c6c50168c77
rohaninheritcloud <rohanr.kucchukulla@gmail.com>
2024-04-23 22:35:44 +0530

    changed the api protocol for tracking details retrieval

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ const OrderForm = () => {
     }
 
     try {
-      const response = await fetch("http://azure-storage-icloud.us-e2.cloudhub.io/azure/downloadBlob",{
+      const response = await fetch("https://azure-storage-icloud.us-e2.cloudhub.io/azure/downloadBlob",{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(trackingData),
